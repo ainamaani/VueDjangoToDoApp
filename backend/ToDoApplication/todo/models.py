d7f51e172fa1f962e22ebd55704aa8d0d4595423
Ainamaani Isaac <aina.isaac2002@gmail.com>
2023-11-30 23:46:21 +0300

    Project set up

---
+++ backend/ToDoApplication/todo/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Task (models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=10)
+    description = models.CharField(max_length=300)
+    completed = models.BooleanField(default=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
